cfe4e4583cf10f85b9ae1c2e8c7b86f1f248a69e
Francis Ofori Anane <ofori.anane@gmail.com>
2023-09-29 17:29:18 +0000

    Add file

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,5 +1,20 @@
 import Building from './5-building';
+/* eslint-disable no-underscore-dangle */
+export default class SkyHighBuilding extends Building {
+  constructor(sqft = 0, floors = 0) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-export default SkyHighBuilding extends Building{
-    
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
 }
