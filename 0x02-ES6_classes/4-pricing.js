e7a493275729fe82b215811c717c95c3c1ac0205
Francis Ofori Anane <ofori.anane@gmail.com>
2023-09-29 14:39:07 +0000

    Modify class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} (${this._currency.displayFullCurrency()})`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount = 0, conversionRate = 0.0) {
