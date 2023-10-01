54515c8c920582bef5065b09d33c7c868c1adbb9
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-01 17:22:52 +0000

    modified:   100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(range, brand, motor, color) {
-    super(brand, motor, color)
+    super(brand, motor, color);
     this._range = range;
   }
 
