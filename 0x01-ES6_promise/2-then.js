6b55bd18e6f789cd5ec680427b6d7ce0b75abeee
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-02 01:19:48 +0000

    modified:   2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -6,5 +6,5 @@ export default function handleResponseFromAPI(promise) {
   }).then(() => {
     console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
-  }).catch(new Error());
+  }).catch(() => new Error());
 }
