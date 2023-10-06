b74d2bbfa75aef873aa8d9cada10e4818d99f554
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-06 13:16:26 +0000

    modified:   main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,50 @@ interface Teacher{
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any;
 }
+
+
+interface Directors extends Teacher{
+    numberOfReports: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+interface printTeacherFunction{
+    // define a signature for a function that takes sting params and returns a string 
+    (firstName: string, lastName: string): string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    const firstInitial = firstName.charAt(0);
+    return `${firstInitial}. ${lastName}`;
+}
+
+interface StudentInterface {
+    // Signature for StudentClass
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+        
+    workOnHomework(): string{
+        return 'Currently working';
+    }
+    displayName(): string{
+        return this.firstName;
+    }
+}
+
+// Example usage:
+const student = new StudentClass("John", "Doe");
+console.log(student.workOnHomework()); // Output: Currently working
+console.log(student.displayName());     // Output: John
