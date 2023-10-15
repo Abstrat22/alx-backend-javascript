d52ade15f4f703650dade28d3486ddb87c8df170
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-15 16:30:16 +0000

    modified:   5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const arrayBuffer = new ArrayBuffer(length);
-  // A view instance to work with Int8 datatypes on the the Allocated buffer
-  const int8view = new Int8Array(arrayBuffer);
   if (length < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-  int8view[position] = value;
-  return arrayBuffer;
+  // A view instance to work with Int8 datatypes on the the Allocated buffer
+  const dataView = new DataView(arrayBuffer);
+  dataView.setInt8(position, value); // set data on the allocated buffer
+
+  return dataView;
 }
