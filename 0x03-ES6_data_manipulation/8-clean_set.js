fcd5999347cd1c02c7d92a08c26053c89f643d29
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-17 23:03:56 +0000

    modified:   8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,10 @@ export default function cleanSet(set, startString) {
   if (typeof startString === 'string') {
     const filteredSet = [...set]
       .filter((value) => value
-        .startsWith(startString) && startString !== '');
-    return filteredSet.join('-')
-      .replace(new RegExp(startString, 'g'), '');
+        .startsWith(startString) && startString !== '')
+      .map((value) => value
+           .replace(new RegExp(`^${startString}`), '')).join('-');
+    return filteredSet;
   }
   return '';
 }
