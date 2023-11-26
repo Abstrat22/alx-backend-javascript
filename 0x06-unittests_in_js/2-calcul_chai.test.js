08bbdaeeafb67617d4bc35ee3736e7f846027de7
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-26 22:50:19 +0000

    Modify file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,27 +7,21 @@ const calculateNumber = require("./1-calcul");
 describe("calculateNumber()", function() {
 
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUM", 1, 2);
-      expect(res).to.be.equal(3);
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      expect(res).to.be.equal(-1);;
+      expect(calculateNumber("SUBTRACT", 1.4, 2.2)).to.equal(-1);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      expect(res).to.be.equal(2);;
+      expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 4, 2);
-      expect(res).to.be.equal(2);
+      expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 1.7, 0);
-      expect(res).to.be.equal("Error");
+      expect(calculateNumber("DIVIDE", 1.7, 0)).to.equal("Error");
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      expect(res).to.be.equal(0.2);;
+      expect(calculateNumber("DIVIDE", 1.4, 4.6)).to.equal(0.2);;
     });
 });
