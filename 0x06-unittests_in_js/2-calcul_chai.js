c4b8c907671b0c7861ef3a1957483fbd3e356562
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-26 22:50:19 +0000

    Modify file

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,16 +1,15 @@
 // round a and b and perform 1 of three operations [SUM, SUBTRACT, DIVIDE]
 function calculateNumber(type, a, b) {
-    let value;
-    if (type === "SUM") {
-        value = ((Math.round(a)) + (Math.round(b)))
+    switch (type) {
+
+        case "SUM":
+            return ((Math.round(a)) + (Math.round(b)));
+        case "SUBTRACT":
+            return ((Math.round(a)) - (Math.round(b)));
+
+        case "DIVIDE":
+            //if b rounds to 0 return Error
+            return Math.round(b) !== 0 ? ((Math.round(a)) / (Math.round(b))) : "Error";
     }
-    if (type === "SUBTRACT") {
-        value = ((Math.round(a)) - (Math.round(b)))
-    }
-    if (type === "DIVIDE") {
-        //if b rounds to 0 the values is set to Error
-        value = Math.round(b) !== 0 ? ((Math.round(a)) / (Math.round(b))) : "Error";
-    }
-    return value;
 }
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,32 +2,58 @@ const chai = require("chai");
 const mocha = require("mocha");
 const expect = chai.expect;
 
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
 describe("calculateNumber()", function() {
 
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUM", 1, 2);
-      expect(res).to.be.equal(3);
+      expect(calculateNumber("SUM", 1, 2)).to.equal(3);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUBTRACT", 1.4, 2.2);
-      expect(res).to.be.equal(-1);;
+      expect(calculateNumber("SUBTRACT", 1.4, 2.2)).to.equal(-1);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("SUBTRACT", 4.9, 2.7);
-      expect(res).to.be.equal(2);;
+      expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 4, 2);
-      expect(res).to.be.equal(2);
+      expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 1.7, 0);
-      expect(res).to.be.equal("Error");
+      expect(calculateNumber("DIVIDE", 1.7, 0)).to.equal("Error");
     });
     it("checking if numbers are rounded", function() {
-      const res = calculateNumber("DIVIDE", 1.4, 4.6);
-      expect(res).to.be.equal(0.2);;
+      expect(calculateNumber("DIVIDE", 1.4, 4.6)).to.equal(0.2);;
     });
+});
+describe("calculateNumber", function() {
+  describe("When type is SUM", function() {
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("SUM", 1, 2)).to.equal(3);
+});
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("SUM", 1.6, 2.6)).to.equal(5);
+});
+  });
+  describe("When type is SUBTRACT", function() {
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("SUBTRACT", 1.4, 2.3)).to.equal(-1);
+});
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("SUBTRACT", 4.9, 2.7)).to.equal(2);
+});
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("SUBTRACT", -4.9, -2.7)).to.equal(-2);
+});
+  });
+  describe("When type is DIVIDE", function() {
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("DIVIDE", 4, 2)).to.equal(2);
+});
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("DIVIDE", 4.6, 1.8)).to.equal(2.5);
+});
+it("checking if numbers are rounded", function() {
+    expect(calculateNumber("DIVIDE", 4, 0)).to.equal("Error");
+});
+  });
 });
