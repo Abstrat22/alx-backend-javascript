0a07eb2f0d5b2fbc58190a32730da7c13d5bcac3
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-26 22:41:15 +0000

    Modify files

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,13 +1,13 @@
 // round a and b and perform 1 of three operations [SUM, SUBTRACT, DIVIDE]
 function calculateNumber(type, a, b) {
     let value;
-    if (type == "SUM") {
+    if (type === "SUM") {
         value = ((Math.round(a)) + (Math.round(b)))
     }
-    if (type == "SUBTRACT") {
+    if (type === "SUBTRACT") {
         value = ((Math.round(a)) - (Math.round(b)))
     }
-    if (type == "DIVIDE") {
+    if (type === "DIVIDE") {
         //if b rounds to 0 the values is set to Error
         value = Math.round(b) !== 0 ? ((Math.round(a)) / (Math.round(b))) : "Error";
     }
